1ef66458a6b429819e5009447839828d7c89857f
Abhijeet Verma <vickyverma4u@gmail.com>
2021-05-22 19:59:32 +0530

    implemented textfield styling

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,51 +1,64 @@
-import React, { Component } from 'react';
-import './Header.css';
-import Grid from '@material-ui/core/Grid';
-import SearchIcon from '@material-ui/icons/Search';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import { green, red } from '@material-ui/core/colors';
+import React, { Component } from "react";
+import "./Header.css";
+import Grid from "@material-ui/core/Grid";
+import SearchIcon from "@material-ui/icons/Search";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { withStyles } from "@material-ui/core/styles";
+import { green, red } from "@material-ui/core/colors";
 
+const styles = (theme) => ({
+  root: {
+    "& .MuiInput-underline:before": {
+      borderBottomColor: "black",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "white",
+    },
+    "& .MuiInputBase-input": {
+      color: "white",
+    },
+  },
+});
 
 class Header extends Component {
+  constructor() {
+    super();
+    // variable to check, if the options menu
+    // is open or not
+    this.state = {
+      "options-open": false,
+    };
+  }
 
-    constructor() {
-        super();
-        // variable to check, if the options menu
-        // is open or not
-        this.state = {
-            "options-open": false,
-        }
-    }
+  loginHandler = () => {
+    this.props.modalHandler.openModalHandler();
+  };
 
-    loginHandler = () => {
-        this.props.modalHandler.openModalHandler();
+  picClickHandler = () => {
+    if (this.state["options-open"]) {
+      this.setState({ "options-open": false });
+    } else {
+      this.setState({ "options-open": true });
     }
+  };
 
-    picClickHandler = () => {
-        if (this.state['options-open']) {
-            this.setState({ 'options-open': false });
-        }
-        else {
-            this.setState({ 'options-open': true });
-        }
-    }
+  filterRestaurantHandler = (e) => {
+    this.props.modalHandler.filterRestaurant(e.target.value);
+  };
 
-    filterRestaurantHandler = (e) => {
-        this.props.modalHandler.filterRestaurant(e.target.value);
-    }
+  logoutClickHandler = () => {
+    this.props.modalHandler.logout();
+    this.setState({ "options-open": false });
+  };
 
-    logoutClickHandler = () => {
-        this.props.modalHandler.logout();
-        this.setState({ 'options-open': false });
-    }
-
-    profilePageNavigationHandler = () => {
-        this.props.modalHandler.goToProfile();
-    }
-    /*console.log(sessionStorage.getItem('access-token'));
+  profilePageNavigationHandler = () => {
+    this.props.modalHandler.goToProfile();
+  };
+  /*console.log(sessionStorage.getItem('access-token'));
     sessionStorage.removeItem('uuid');
     sessionStorage.removeItem('access-token');
     this.setState({ loggedIn: false })
@@ -53,54 +66,85 @@ class Header extends Component {
     this.props.history.push('/');
     */
 
-    render() {
-
-        return <div className="header-outer-div">
-            <div className="header-grid-div">
-                <Grid container>
-                    <Grid item xs={12} sm={12} md={4}>
-                        <div className="logo-icon-div"><FastfoodIcon /></div>
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={7}>
-                    {(this.props.isOnHome) &&<div><div className="header-search_field-container">
-                            <SearchIcon />
-                            <TextField style={{width:210}}
-                                placeholder="Search by Restaurant name" onChange={this.filterRestaurantHandler}/>
-                        </div></div>}
-                    </Grid>
-                    <Grid item xs={12} sm={12} md={1}>
-                    {!(this.props.isLogin) && <div className="header-login-btn-div">
-                            <Button
-                                variant="contained"
-                                color="default"
-                                startIcon={<AccountCircleIcon />}
-                                onClick={this.loginHandler}
-                                style={{width:90, left:5}}
-                            >
-                                Login
-      </Button>
-                        </div>}
-                        {(this.props.isLogin) && <div onClick={this.picClickHandler} className="header-login-btn-div">
-                            <div className="logged-in-user-first-name-div">
-                            <AccountCircleIcon/>
-                            <p className="loggedInUserName-para">{this.props.firstName}</p>
-                            </div>
-                            
-                        </div>}
-                    </Grid>
-                </Grid>
-            </div>
-            {this.state['options-open'] && <div>
-                    <div className="account-options-container">
-                    <div>
-                            <p onClick={this.profilePageNavigationHandler} >My Profile</p>
-                        </div>
-                        <p onClick={this.logoutClickHandler}>Logout</p>
-                    </div>
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="header-outer-div">
+        <div className="header-grid-div">
+          <Grid container>
+            <Grid item xs={12} sm={12} md={4}>
+              <div className="logo-icon-div">
+                <FastfoodIcon fontSize="large" />
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={12} md={7}>
+              {this.props.isOnHome && (
+                <div>
+                  <div className="header-search_field-container">
+                    {/* <SearchIcon /> */}
+                    <TextField
+                      style={{ width: 300 }}
+                      placeholder="Search by Restaurant name"
+                      onChange={this.filterRestaurantHandler}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <SearchIcon htmlColor="white" />
+                          </InputAdornment>
+                        ),
+                      }}
+                      classes={{
+                        root: classes.root,
+                      }}
+                    />
+                  </div>
                 </div>
-                }
+              )}
+            </Grid>
+            <Grid item xs={12} sm={12} md={1}>
+              {!this.props.isLogin && (
+                <div className="header-login-btn-div">
+                  <Button
+                    variant="contained"
+                    color="default"
+                    startIcon={<AccountCircleIcon />}
+                    onClick={this.loginHandler}
+                    style={{ width: 90, left: 5 }}
+                  >
+                    Login
+                  </Button>
+                </div>
+              )}
+              {this.props.isLogin && (
+                <div
+                  onClick={this.picClickHandler}
+                  className="header-login-btn-div"
+                >
+                  <div className="logged-in-user-first-name-div">
+                    <AccountCircleIcon />
+                    <p className="loggedInUserName-para">
+                      {this.props.firstName}
+                    </p>
+                  </div>
+                </div>
+              )}
+            </Grid>
+          </Grid>
         </div>
-    }
+        {this.state["options-open"] && (
+          <div>
+            <div className="account-options-container">
+              <div>
+                <p onClick={this.profilePageNavigationHandler}>My Profile</p>
+              </div>
+              <p onClick={this.logoutClickHandler}>Logout</p>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default Header;
+// export default Header;
+export default withStyles(styles, { withTheme: true })(Header);
--- src/screens/restaurantDetailsSection/RestaurantDetails.js
+++ src/screens/restaurantDetailsSection/RestaurantDetails.js
@@ -1,114 +1,96 @@
-import React from 'react';
-import './RestaurantDetails.css';
-import StarIcon from '@material-ui/icons/Star';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import "./RestaurantDetails.css";
+import StarIcon from "@material-ui/icons/Star";
+import Grid from "@material-ui/core/Grid";
 function RestaurantDetails(props) {
-return <div className="restaurant-info-section">
-<Grid container>
-    <Grid item xs={12} sm={12} md={3}>
-
-        <div className="restaurant-info-image-div">
-
-
-            <Grid container>
-                <Grid item xs={4} sm={4} md={0}></Grid>
+  return (
+    <div className="restaurant-info-section">
+      <Grid container spacing={2}>
+        {/* <Grid item xs={12} sm={12} md={3}> */}
+        {/* <div className="restaurant-info-image-div"> */}
+        {/* <Grid container> */}
+        {/* <Grid item xs={4} sm={4} md={0}></Grid> */}
+        <Grid item xs={12} sm={12} md={3} className="restaurant-grid">
+          <img
+            className="restaurant-info-image"
+            src={props.restaurant.photo_URL}
+            alt={props.restaurant.restaurant_name}
+          />
+        </Grid>
+        {/* <Grid item xs={4} sm={4} md={0}></Grid> */}
+        {/* </Grid> */}
+        {/* </div> */}
+        {/* </Grid> */}
+        <Grid item xs={12} sm={12} md={9}>
+          <div>
+            <div>
+              <Grid container>
+                <Grid item xs={4} sm={4} md={0} />
                 <Grid item xs={4} sm={4} md={12}>
-                    <img className="restaurant-info-image" src={props.restaurant.photo_URL} alt={props.restaurant.restaurant_name} />
+                  <p>{props.restaurant.restaurant_name}</p>
                 </Grid>
-                <Grid item xs={4} sm={4} md={0}></Grid>
-            </Grid>
-        </div>
-    </Grid>
-    <Grid item xs={12} sm={12} md={9}>
-        <div>
-            <div>
-                <Grid container>
-                    <Grid item xs={4} sm={4} md={0}></Grid>
-                    <Grid item xs={4} sm={4} md={12}>
-                        <p>
-                            {props.restaurant.restaurant_name}
-                </p>
-                    </Grid>
-                    <Grid item xs={4} sm={4} md={0}></Grid>
-                </Grid>
-
+                <Grid item xs={4} sm={4} md={0} />
+              </Grid>
             </div>
             <div>
-
-                <Grid container>
-                    <Grid item xs={4} sm={4} md={0}></Grid>
-                    <Grid item xs={4} sm={4} md={12}>
-                        <p>{props.restaurant.address.city}</p>
-                    </Grid>
-                    <Grid item xs={4} sm={4} md={0}></Grid>
+              <Grid container>
+                <Grid item xs={4} sm={4} md={0} />
+                <Grid item xs={4} sm={4} md={12}>
+                  <p>{props.restaurant.address.city}</p>
                 </Grid>
-
-
+                <Grid item xs={4} sm={4} md={0} />
+              </Grid>
             </div>
             <div>
-                <Grid container>
-                    <Grid item xs={4} sm={4} md={0}></Grid>
-                    <Grid item xs={4} sm={4} md={12}>
-                        <p>{props.categoryList}</p>
-                    </Grid>
-                    <Grid item xs={4} sm={4} md={0}></Grid>
+              <Grid container>
+                <Grid item xs={4} sm={4} md={0} />
+                <Grid item xs={4} sm={4} md={12}>
+                  <p>{props.categoryList}</p>
                 </Grid>
-
+                <Grid item xs={4} sm={4} md={0} />
+              </Grid>
             </div>
             <div className="restaurant-info-customer-rating-cost-div">
-
-
-
-                <Grid container>
-                    <Grid item xs={6} sm={6} md={3}>
-                        <div>
-                            <Grid container>
-                                <Grid item xs={3} sm={3} md={0}>
-                                </Grid>
-                                <Grid item xs={3} sm={3} md={12}>
-                                    <StarIcon />
-                                    <p>{props.restaurant.customer_rating}</p>
-                                    <p>AVERAGE RATING BY</p>
-                                    <p>{props.restaurant.number_customers_rated+" CUSTOMERS"}</p>
-                                </Grid>
-                                <Grid item xs={3} sm={3} md={0}>
-
-                                </Grid>
-                            </Grid>
-                        </div>
+              <Grid container>
+                <Grid item xs={6} sm={6} md={3}>
+                  <div>
+                    <Grid container>
+                      <Grid item xs={3} sm={3} md={0} />
+                      <Grid item xs={3} sm={3} md={12}>
+                        <StarIcon />
+                        <p>{props.restaurant.customer_rating}</p>
+                        <p>AVERAGE RATING BY</p>
+                        <p>
+                          {props.restaurant.number_customers_rated +
+                            " CUSTOMERS"}
+                        </p>
+                      </Grid>
+                      <Grid item xs={3} sm={3} md={0} />
                     </Grid>
-                    <Grid item xs={0} sm={0} md={3}>
-                    </Grid>
-                    <Grid item xs={6} sm={6} md={3}>
-                        <div>
-                            <Grid container>
-                                <Grid item xs={3} sm={3} md={0}>
-
-                                </Grid>
-                                <Grid item xs={3} sm={3} md={12}>
-                                    <p>{"₹ "+props.restaurant.average_price}</p>
-                                    <p>AVERAGE COST FOR</p>
-                                    <p>TWO PEOPLE</p>
-                                </Grid>
-                                <Grid item xs={3} sm={3} md={0}>
-
-                                </Grid>
-                            </Grid>
-                        </div>
-                    </Grid>
-                    <Grid item xs={0} sm={0} md={3}>
-                    </Grid>
+                  </div>
                 </Grid>
-
-
-
-
+                <Grid item xs={0} sm={0} md={3} />
+                <Grid item xs={6} sm={6} md={3}>
+                  <div>
+                    <Grid container>
+                      <Grid item xs={3} sm={3} md={0} />
+                      <Grid item xs={3} sm={3} md={12}>
+                        <p>{"₹ " + props.restaurant.average_price}</p>
+                        <p>AVERAGE COST FOR</p>
+                        <p>TWO PEOPLE</p>
+                      </Grid>
+                      <Grid item xs={3} sm={3} md={0} />
+                    </Grid>
+                  </div>
+                </Grid>
+                <Grid item xs={0} sm={0} md={3} />
+              </Grid>
             </div>
-        </div>
-
-    </Grid>
-</Grid>
-</div>;
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default RestaurantDetails;
